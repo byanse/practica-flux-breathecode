0f8641c20d3fd407e3ec689ca44048a22710459f
Bárbara Ulloa <ulloaby@gmail.com>
2021-05-12 17:48:00 -0400

    practica finalizada

---
+++ src/js/components/ContactCard.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class ContactCard extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            // initialize your state
+        };
+    }
+    
+    render(){
+        return (
+            <li className="list-group-item">
+                <div className="row w-100">
+                    <div className="col-12 col-sm-6 col-md-3 px-0">
+                        <img src="http://demos.themes.guide/bodeo/assets/images/users/m101.jpg" alt="Mike Anamendolla" className="rounded-circle mx-auto d-block img-fluid" />
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
+                        <div className=" float-right">
+                            <button className="btn" onClick={() => this.props.history.push("/edit/"+this.props.data.id)}><i className="fas fa-pencil-alt mr-3"></i></button>
+                            <button className="btn" onClick={() => this.props.onDelete(this.props.data)}><i className="fas fa-trash-alt"></i></button>
+                        </div>
+                        <label className="name lead">{this.props.data.full_name}</label>
+                    </div>
+                </div>
+            </li>
+        );
+    }
+    
+}
+
+/**
+ * here is where you define the data-types for
+ * your component propersties
+**/
+ContactCard.propTypes = {
+    history: PropTypes.object,
+    onDelete: PropTypes.func,
+    data: PropTypes.object
+};
+
+/**
+ * here is where you define the default values
+ * for your component propersties
+**/
+ContactCard.defaultProps = {
+  onDelete: null
+};
+export default withRouter(ContactCard);
---
+++ src/js/stores/store.js
@@ -0,0 +1,19 @@
+import Flux from "@4geeksacademy/react-flux-dash";
+
+class Store extends Flux.DashStore{
+    constructor (){
+        super();
+        this.addEvent("contacts", (data)=>{
+            //transform the data
+            if(!Array.isArray(data)) return data;
+            const contacts = data.map((c) => { 
+                c.id = Math.floor(Math.random() * 9999999999);
+                return c;
+            });
+            return contacts;
+        });
+        
+}
+}
+
+export default new Store();
